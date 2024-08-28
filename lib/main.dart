897155868f060e1e7a909a6adcc375156ca29caf
Jonathan Yeung <yeung@guppy.co.jp>
2024-08-28 18:08:17 +0900

    session 3: navigation and back

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
+import 'weather.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(home: MainApp()));
 }
 
 class MainApp extends StatefulWidget {
@@ -16,112 +15,29 @@ class MainApp extends StatefulWidget {
 }
 
 class MainAppState extends State<MainApp> {
-  String condition = '';
+  @override
+  void initState() {
+    super.initState();
 
-  void getWeather() {
-    final yumemi = YumemiWeather();
-
-    setState(() {
-      condition = yumemi.fetchSimpleWeather();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(milliseconds: 500), () async {
+        if (mounted) {
+          await Navigator.push(
+            context,
+            MaterialPageRoute<WeatherScreen>(
+              builder: (context) => const WeatherScreen(),
+            ),
+          );
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              final width = constraints.maxWidth * 0.5;
-              final themeData = Theme.of(context).textTheme;
-
-              return Container(
-                constraints: BoxConstraints(
-                  maxWidth: width,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      constraints: BoxConstraints(
-                        maxWidth: width,
-                        maxHeight: width,
-                      ),
-                      child: (condition == '')
-                          ? const Placeholder(
-                              color: Colors.black12,
-                            )
-                          : SvgPicture.asset(
-                              width: width,
-                              height: width,
-                              'assets/$condition.svg',
-                            ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 16,
-                          ),
-                          child: FittedBox(
-                            child: Text(
-                              '** ℃',
-                              textAlign: TextAlign.center,
-                              style: themeData.labelLarge!
-                                  .copyWith(color: Colors.blue),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 16,
-                          ),
-                          child: FittedBox(
-                            child: Text(
-                              '** ℃',
-                              textAlign: TextAlign.center,
-                              style: themeData.labelLarge!
-                                  .copyWith(color: Colors.red),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 80,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        FittedBox(
-                          child: Text(
-                            'Close',
-                            textAlign: TextAlign.center,
-                            style: themeData.labelLarge!
-                                .copyWith(color: Colors.blue),
-                          ),
-                        ),
-                        FittedBox(
-                          child: GestureDetector(
-                            onTap: getWeather,
-                            child: Text(
-                              'Reload',
-                              textAlign: TextAlign.center,
-                              style: themeData.labelLarge!
-                                  .copyWith(color: Colors.blue),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
+    return Scaffold(
+      body: Container(
+        color: Colors.green,
       ),
     );
   }
---
+++ lib/weather.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({
+    super.key,
+  });
+
+  @override
+  WeatherScreenState createState() => WeatherScreenState();
+}
+
+class WeatherScreenState extends State<WeatherScreen> {
+  String condition = '';
+
+  void getWeather() {
+    final yumemi = YumemiWeather();
+
+    setState(() {
+      condition = yumemi.fetchSimpleWeather();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final width = constraints.maxWidth * 0.5;
+            final themeData = Theme.of(context).textTheme;
+
+            return Container(
+              constraints: BoxConstraints(
+                maxWidth: width,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    constraints: BoxConstraints(
+                      maxWidth: width,
+                      maxHeight: width,
+                    ),
+                    child: (condition == '')
+                        ? const Placeholder(
+                            color: Colors.black12,
+                          )
+                        : SvgPicture.asset(
+                            width: width,
+                            height: width,
+                            'assets/$condition.svg',
+                          ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 16,
+                        ),
+                        child: FittedBox(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: themeData.labelLarge!
+                                .copyWith(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 16,
+                        ),
+                        child: FittedBox(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: themeData.labelLarge!
+                                .copyWith(color: Colors.red),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FittedBox(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Close',
+                            textAlign: TextAlign.center,
+                            style: themeData.labelLarge!
+                                .copyWith(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                      FittedBox(
+                        child: GestureDetector(
+                          onTap: getWeather,
+                          child: Text(
+                            'Reload',
+                            textAlign: TextAlign.center,
+                            style: themeData.labelLarge!
+                                .copyWith(color: Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
